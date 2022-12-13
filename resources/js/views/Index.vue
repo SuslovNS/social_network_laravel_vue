d7033661e8cb4d427d7eeb2df94272e36c1e14a8
SuslovNS <nikitka536@mail.ru>
2022-12-13 22:10:20 +0500

    init

---
+++ resources/js/views/Index.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    index
+</div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    // data(){
+    //     return {
+    //         token:null
+    //     }
+    // },
+    //
+    // mounted() {
+    //     this.getToken()
+    // },
+    //
+    // watch: {
+    //     $route(to, from){
+    //         this.getToken()
+    //     }
+    // },
+    //
+    // methods: {
+    //     getToken(){
+    //         this.token = localStorage.getItem('x_xsrf_token')
+    //     },
+    //
+    //     logout(){
+    //         axios.post('/logout')
+    //             .then( res => {
+    //                 localStorage.removeItem('x_xsrf_token')
+    //                 this.$router.push({name: 'user_login'})
+    //             })
+    //     }
+    // }
+}
+</script>
+
+<style scoped>
+
+</style>
