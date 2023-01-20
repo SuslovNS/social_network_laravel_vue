5e9f34df8af7e85e76e8f1f4bcfa6e44c835d507
SuslovNS <nikitka536@mail.ru>
2023-01-20 16:02:42 +0500

    beta 1.0(build)

--- resources/js/views/user/Login.vue
+++ resources/js/views/user/Login.vue
@@ -7,7 +7,7 @@
             <input v-model="password" type="password" placeholder="password" class="w-96 p-1 mb-2 border border-inherit rounded-lg">
         </div>
         <div>
-            <input @click.prevent="login" type="submit" value="login" class="block float-right mx-auto w-32 p-1 bg-sky-400 text-white rounded-lg">
+            <input @click.prevent="login" type="submit" value="login" class="block float-right mx-auto w-32 p-1 bg-red-600 text-white rounded-lg">
         </div>
     </div>
 </template>
